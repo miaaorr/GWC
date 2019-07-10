7e034b860aadfaa93ca913f2052de9a82788c74e
Mia Orr <52763242+miaaorr@users.noreply.github.com>
2019-07-10 15:15:57 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,15 @@
+
+onopen = window.alert("Warning: You are about to experience an extreme burst of happiness. Click okay to continue")
+
+
+
+
+
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
+}
